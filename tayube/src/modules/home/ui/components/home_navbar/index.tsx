f30bda2779289d282e8a089cfb7622d3d8e287d0
Anwar675 <151812885+Anwar675@users.noreply.github.com>
2025-07-24 10:53:06 +0000

    a few fix

--- tayube/src/modules/home/ui/components/home_navbar/index.tsx
+++ tayube/src/modules/home/ui/components/home_navbar/index.tsx
@@ -8,7 +8,7 @@ import { AuthButton } from "@/modules/auth/ui/components/auth_button"
 export const HomeNavbar = () => {
     return (
         <div className="fixed top-0 left-0 right-0 h-16 bg-[#0f0f0f] flex items-center px-2 pr-5 z-50">
-            <div className="flex items-center gap-4 w-full">
+            <div className="flex sm:items-center justify-between gap-4 w-full">
                 {/*  Menu and Logo  */}
                 <div className="flex items-center shrink-0">
 
@@ -21,10 +21,10 @@ export const HomeNavbar = () => {
                     </Link>
                 </div>
                 {/* Search bar */}
-                <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
+                <div className="flex-1 sm:flex hidden justify-center max-w-[720px] mx-auto">
                     <SearchInput />
                 </div>
-                <div className="flex flex-end flex-shrink-0 items-center  gap-4">
+                <div className="flex  flex-end flex-shrink-0 items-center  gap-4">
                     <AuthButton />
                 </div>
             </div>
