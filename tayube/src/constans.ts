c96b68b061f48a18e37f3f48779ec51cb7d55403
Anwar <doanthienan54@gmail.com>
2025-09-01 16:25:37 +0000

    change

--- tayube/src/constans.ts
+++ tayube/src/constans.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_LIMIT = 5
-export const APP_URL = process.env.VERCEL_URL || 'http://localhost:3000'
+export const APP_URL = process.env.NEXT_PUBLIC_APP || 'http://localhost:3000'
